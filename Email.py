5615dfaaaa57c14789bbd970a899d4e21d46d0f6
DQIris <ldq_2014@163.com>
2020-08-28 13:40:42 +0800

    'Email'

--- Email.py
+++ Email.py
@@ -6,11 +6,9 @@ import csv
 from email.mime.text import MIMEText
 from email.utils import formataddr
 
-my_sender = '1622898550@qq.com'  # 发件人邮箱账号
-my_pass = 'xxxxxxxxxxxx'  # 发件人邮箱密码
-
-
-def mail():
+def sendEmail(websiteName):
+    my_sender = '1622898550@qq.com'  # Sender's email account
+    my_pass = 'ldxhanniriayebga'  # Sender's mailbox password
     ret = True
     try:
         clientList = []
@@ -20,24 +18,25 @@ def mail():
                 clientList.append(line)
                 if line[0].isdigit():
                     my_user = line[2]
-                    msg = MIMEText('Dear '+line[1], 'plain', 'utf-8')
-                    msg['From'] = formataddr(["From CITI", my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
-                    msg['To'] = formataddr(["FK", my_user])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
-                    msg['Subject'] = "CITI"  # 邮件的主题，也可以说是标题
+                    msg = MIMEText('Dear '+line[1]+',\n\n        Sorry to inform'\
+                          ' you that the website \"'+ websiteName +'\" failed to access.\n\nBest wishes\nGroup 13', 'plain', 'utf-8')
+                    msg['From'] = formataddr(["Group 13", my_sender])  # sender’s email nickname and sender’s email account
+                    msg['To'] = formataddr(["FK", my_user])  # recipient's email nickname, recipient's email account
+                    msg['Subject'] = "Website \"" + websiteName + "\" request failed [Remind from Group 13]"  # Email Subject
 
-                    server = smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25
-                    server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
-                    server.sendmail(my_sender, [my_user, ], msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
-                    server.quit()  # 关闭连接
-
-
-    except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+                    server = smtplib.SMTP_SSL("smtp.qq.com", 465)  # The SMTP server in the sender's mailbox, the port is 25
+                    server.login(my_sender, my_pass)  # Sender's email account and email password
+                    server.sendmail(my_sender, [my_user, ], msg.as_string())  # Sender's email account, recipient's email account, sending email
+                    server.quit()  # Close the connection
+    except Exception:
         ret = False
     return ret
 
-ret = mail()# 收件人邮箱账号
+# Test
+ret = sendEmail('www.xxx.com')
 
 if ret:
-    print("邮件发送成功")
+    print("Email sent successfully")
 else:
-    print("邮件发送失败")
+    print("Email failed to send")
+
